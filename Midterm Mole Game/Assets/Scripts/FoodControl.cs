3defb78b3b8c44aba017293e3b4633f76d218df6
Jeremy Huml <jh5293@nyu.edu>
2020-03-04 20:50:07 -0500

    Wrote code for prey creature
    -Prey can now move
    -Prey can dig through dirt upon spawning
    -Prey can dig through dirt continuously if manually selected to

---
+++ Midterm Mole Game/Assets/Scripts/FoodControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodControl : MonoBehaviour
+{
+    public bool CanDig = true;
+    public NPCDigger digger;
+    public float DigDelayTime = 0.2f;
+    public float DigTimer = 0;
+
+    public float MoveDelayTime = 2f;
+    public float MoveTimer = 0;
+    public float MaxMoveTime = 5f;
+    public float MinMoveTime = 1f;
+
+    public float MovementSpeed = 1f;
+    public Vector3 Direction = Vector3.zero;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+        DigTimer = DigDelayTime;
+        ChooseDirection();
+        SetMoveTimer();
+    }
+
+    void SetMoveTimer()
+    {
+        MoveDelayTime = Random.Range(MinMoveTime, MaxMoveTime);
+        MoveTimer = MoveDelayTime;
+    }
+
+    void Update()
+    {
+        if (DigTimer > 0)
+        {
+            DigTimer -= Time.deltaTime;
+            if (DigTimer <= 0) digger.CanDig = CanDig;
+        }
+        if (MoveTimer > 0)
+        {
+            MoveInDirection();
+            MoveTimer -= Time.deltaTime;
+            if (MoveTimer <= 0)
+            {
+                ChooseDirection();
+                SetMoveTimer();
+            }
+        }
+        else
+        {
+            ChooseDirection();
+            SetMoveTimer();
+        }
+    }
+
+    void ChooseDirection()
+    {
+        Direction = Random.onUnitSphere;
+        Direction = new Vector3(Direction.x, Direction.y, 0);
+    }
+
+    void MoveInDirection()
+    {
+        rb.AddForce(Direction * MovementSpeed * Time.deltaTime);
+    }
+}
---
+++ Midterm Mole Game/Assets/Scripts/NPCDigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDigger : MonoBehaviour
+{
+    //Determines whether or not the NPC in question can dig
+    public bool CanDig = true;
+
+    void Update()
+    {
+        //Keeps in the same location as parent object, since it wasn't doing that already
+        transform.position = transform.parent.position;
+    }
+
+    //Destroys dirt
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CanDig && collision.gameObject.CompareTag("Dirt"))
+        {
+            collision.gameObject.SendMessage("DestroyDirt");
+        }
+    }
+}
