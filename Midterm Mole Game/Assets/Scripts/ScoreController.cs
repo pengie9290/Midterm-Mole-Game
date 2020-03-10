8788f8e9ddfd6cca57a1ec4e2df0fdf0a279a156
Jeremy Huml <jh5293@nyu.edu>
2020-03-09 21:34:02 -0400

    Created Scorekeeping
    Implemented scorekeeping functionality

---
+++ Midterm Mole Game/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public static ScoreController Instance = null;
+
+    private bool _gameInProgress = false;
+    public bool GameInProgress
+    {
+        get
+        {
+            return _gameInProgress;
+        }
+
+        set
+        {
+            if (!_gameInProgress && value)
+            {
+                Points = 0;
+                AddPoints(0);
+                CurrentTime = MaxTime;
+            }
+            _gameInProgress = value;
+            if (_gameInProgress)
+            {
+                FinalScoreDisplay.text = "";
+            }
+            else
+            {
+                FinalScoreDisplay.text = "Final Score\n" + TotalScore;
+            }
+        }
+    }
+
+    public Text FinalScoreDisplay;
+    public Text PointsDisplay;
+    public Text TimeBonusDisplay;
+        
+    public float Points = 0f;
+    public float MaxTime = 100f;
+    public float CurrentTime = 100f;
+    public float TimePointsMultiplier = 1;
+
+    public int TimeBonus
+    {
+        get
+        {
+            return Mathf.CeilToInt(CurrentTime * TimePointsMultiplier);
+        }
+    }
+
+
+    public float TotalScore
+    {
+        get
+        {
+            return Points + TimeBonus;
+        }
+    }
+
+
+
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else
+        {
+            print("Destroyed");
+            Destroy(gameObject);
+        }
+
+        GameInProgress = true;
+    }
+
+    void Update()
+    {
+        if (GameInProgress && CurrentTime > 0) Countdown();
+    }
+
+    public void AddPoints(float points)
+    {
+        Points += points;
+        PointsDisplay.text = "Points\n" + Points;
+    }
+
+    void Countdown()
+    {
+        CurrentTime -= Time.deltaTime;
+        TimeBonusDisplay.text = "Time Bonus\n" + TimeBonus;
+    }
+}
